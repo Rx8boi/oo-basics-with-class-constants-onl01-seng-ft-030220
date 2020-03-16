046187a305a715c3deaab6d9a155a4b95c371ed4
Charles Butler <cdbutle@outlook.com>
2020-03-16 01:07:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,6 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+BRAND = []
   def initialize(brand)
     @brand = brand
   end
@@ -10,5 +11,7 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
+def brand=(brand)
+  @brand = brand
+  BRAND << brand
 end

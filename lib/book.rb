ab9d82b0dac23651bb9c9d5a122ad44342cc17fc
Charles Butler <cdbutle@outlook.com>
2020-03-16 01:06:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
   attr_accessor :author, :page_count,
-  attr_reader :title
+  attr_reader :title, :genre
 
 GENRES = []
-def genre=(genre)
-  @genre = genre
-  GENRES << genre
-end
+
 
   def initialize(title)
     @title = title
@@ -15,5 +12,10 @@ end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
 
 end
